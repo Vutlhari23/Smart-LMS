489d8271001413742b7581e1a3140e3deae35625
Vutlhari23 <vutlhari23maswanganyi@gmail.com>
2026-04-21 15:55:10 +0200

    Creating a boiler boilerplate for the frontend

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import {Link}  from  "react-router-dom"
+export default function NavBar(){
+
+    return(
+     <>
+       <Link to="/Login">Login</Link>
+
+       <Link to="/register">Register</Link>
+      </>
+     
+    )
+}
